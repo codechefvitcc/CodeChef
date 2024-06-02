0f73f57a954dd9633fec82b573ea5f86ec44e04c
sairamnst <sairamnst1234@gmail.com>
2024-06-02 16:23:26 +0530

    Code for Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
+import { Footer } from "./Components";
+
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+  <div>
+    Hello world
+    <Footer />
+  </div>
+);
 };
 
 export default App;
