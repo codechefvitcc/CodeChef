10a1d394c20344cd05786f5b21e9fbfb9abc4e4d
Spacecentre <akkilalagar05@gmail.com>
2024-06-03 19:38:13 +0530

    Backend Schemas Added

---
+++ server/codechef-vitc-website/schemaTypes/schemas/Faculty.js
@@ -0,0 +1,27 @@
+export default{
+    name : 'faculty',
+    title : 'Faculty',
+    type : 'document',
+    fields : [
+        {
+            name : 'name',
+            title : 'Faculty Name',
+            type : 'string'
+        },
+        {
+            name : 'designation',
+            title : 'Designation',
+            type : 'string'
+        },
+        {
+            name : 'message',
+            title : 'Message for our club',
+            type : 'string'
+        },
+        {
+            name : 'image',
+            title : 'Image',
+            type : 'url'
+        }
+    ]
+}
