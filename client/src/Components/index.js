762a423cca5f83f9837afbf47ce9a58d783c9dcd
Vishal Kumar Yadav <vishal100403@gmail.com>
2024-06-05 22:33:04 +0530

    Hero and Gallery Components created

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,4 +1,6 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Gallery from "./Gallery";
 
-export { Footer, Navbar };
+export { Footer, Navbar, Hero, Gallery };
