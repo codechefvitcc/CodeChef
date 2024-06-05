c45c44fc20adf9e14292e96ef625a4999931e8ad
poseidon0z <adityaprabhu1290@gmail.com>
2024-06-06 00:31:50 +0530

    Added hero section to home page

---
+++ client/src/assets/index.js
@@ -0,0 +1,5 @@
+import vishalpfp from '../assets/vishalKrYadav.jpg';
+import siddhartpfp from '../assets/siddhartTiwari.jpeg';
+import defaultpfp from '../assets/profileDefault.png';
+
+export { vishalpfp, siddhartpfp, defaultpfp };
