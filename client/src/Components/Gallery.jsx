48a44cf76febb5436fb8b2c2a0ed54f2c170c014
sairamnst <sairamnst1234@gmail.com>
2024-06-08 00:57:12 +0530

    added comments to gallery

--- client/src/Components/Gallery.jsx
+++ client/src/Components/Gallery.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
+
+//importing images from assets as an array
 import images from "../assets/gallery";
 
 const Gallery = () => {
+  //using useState to keep track of the current image to display
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  //using useEffect to change the image every 5 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -12,6 +16,7 @@ const Gallery = () => {
     return () => clearInterval(interval);
   }, []);
 
+  //function change image displayed when clicked on the thumbnail
   const handleThumbnailClick = (index) => {
     setCurrentIndex(index);
   };
@@ -19,6 +24,8 @@ const Gallery = () => {
   return (
     <div className="text-center bg-codechef-blue lg:my-10 my-5 px-4">
       <h2 className="text-white font-bold text-4xl my-4 py-[1%]">Gallery</h2>
+
+      {/* Image Banner */}
       <div className="relative lg:w-4/6 mx-auto overflow-hidden xl:w-4/6 h-80 sm:h-52 md:h-64 lg:h-[31.5rem] xl:h-[34rem] rounded-xl shadow-lg" style={{ boxShadow: '0 4px 10px rgba(255, 255, 255, 0.5)' }}>
         {images.map((img, index) => (
           <div
@@ -36,6 +43,7 @@ const Gallery = () => {
         ))}
       </div>
       
+      {/* image thumbnails */}
       <div className="flex justify-center mt-4 space-x-2 py-[1%] overflow-auto">
         {images.map((img, index) => (
           <img
