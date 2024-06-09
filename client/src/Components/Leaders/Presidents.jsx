afcc5c1329a495e9d2ffc84fd0dfc2b478e313a4
poseidon0z <adityaprabhu1290@gmail.com>
2024-06-09 22:53:09 +0530

    Added leaders section

---
+++ client/src/Components/Leaders/Presidents.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Presidents() {
+  return (
+    <div className="w-full md:w-1/2 min-w-[384px] min-h-[250px] h-[33vw]">
+      {/* remove below line */}
+      <div className="w-full h-full bg-amber-600 " />
+    </div>
+  );
+}
+
+export default Presidents;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        'codechef-blue': '#2e3444',
-      },
-    },
-  },
-  plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'codechef-blue': '#2e3444',
+      },
+    },
+  },
+  plugins: [],
 };
