580abd91e52e5ae05932b048ef98d76281b3050c
poseidon0z <adityaprabhu1290@gmail.com>
2024-06-09 22:53:09 +0530

    Added leaders section

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,7 +1,8 @@
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import DeptDisplay from "./Department/Department-Display"
-import Hero from "./Hero";
-import Gallery from "./Gallery";
+import Footer from './Footer';
+import Navbar from './Navbar';
+import DeptDisplay from './Department/Department-Display';
+import Hero from './Hero';
+import Gallery from './Gallery';
+import Leaders from './Leaders/Leaders';
 
-export { Footer, Navbar, DeptDisplay, Hero, Gallery };
+export { Footer, Navbar, DeptDisplay, Hero, Gallery, Leaders };
