da669a2df705f10cc9fc614c4fc01aca3db90018
Akkilesh-A <akkilalagar05@gmail.com>
2024-06-17 18:46:02 +0530

    backend schemas updated

---
+++ server/schemaTypes/schemas/Presidents.js
@@ -0,0 +1,32 @@
+export default{
+    name : 'testimonials',
+    title : 'Testimonials',
+    type : 'document',
+    fields: [
+        {
+            name : 'name',
+            title : 'Name',
+            type : 'string'
+        },
+        {
+            name : 'image',
+            title : 'Image',
+            type : 'url'
+        },
+        {
+            name : 'year',
+            title : 'Year',
+            type : 'string'
+        },
+        {
+            name : 'testimonial',
+            title : 'Testimonial',
+            type : 'string'
+        },
+        {
+            name : 'linkedin',
+            title : 'LinkedIn',
+            type : 'url'
+        }        
+    ]
+}
