7f281de22cb9231773c815baa04e2a7ac8613a00
poseidon0z <adityaprabhu1290@gmail.com>
2024-06-23 00:09:45 +0530

    Fixed merge error

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,10 +1,13 @@
-import Home from "./Home";
+import Home from './Home';
 
 // Events
 import CookOff2024 from './Events/Cookoff2024';
 
-// Blogs
-import Blogs from "./Blogs";
-import BlogPage from "./BlogPage"
+// Leads
+import Batch23 from './Leads/Batch23';
 
-export { Home, Blogs, CookOff2024, BlogPage, Batch23  };
+// Blogs
+import Blogs from './Blogs';
+import BlogPage from './BlogPage';
+
+export { Home, Blogs, CookOff2024, BlogPage, Batch23 };
