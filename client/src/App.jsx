d847fa140e9dfa792f82027202bb1014b7b0dd99
sairamnst <sairamnst1234@gmail.com>
2024-06-23 20:23:20 +0530

    changed navbar and footer routes and fixed navbar for mobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Footer, Navbar, ScrollToTop } from "./Components";
-import { Blogs, CookOff2024, Home, BlogPage, Batch23  } from "./Pages";
+import { Blogs, CookOff2024, DecodeDSA, Home, BlogPage, Batch23  } from "./Pages";
 import JoinUs from "./Pages/JoinUs";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -13,9 +13,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<BlogPage />} />
-        <Route path="/events/Cookoff2024" element={<CookOff2024 />} />
+        <Route path="/events/Cook-Off-2024" element={<CookOff2024 />} />
+        <Route path="/events/Decode-DSA" element={<DecodeDSA />} />
         <Route path="/join-us" element={<JoinUs />} />
-        <Route path="/leads" element={<Batch23 />} />
+        <Route path="/our-teams" element={<Batch23 />} />
       </Routes>
       <Footer />
     </Router>
