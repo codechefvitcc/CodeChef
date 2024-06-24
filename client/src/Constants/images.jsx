6361d8e76333948b42b557bf9f69907f2e29ffc9
sairamnst <sairamnst1234@gmail.com>
2024-06-24 17:18:37 +0530

    added blurhash to faculty

--- client/src/Constants/images.jsx
+++ client/src/Constants/images.jsx
@@ -19,6 +19,10 @@ import vishalpfp from '../assets/vishalKrYadav.jpg';
 import CookOff2024Main from '../assets/Events/CookOff2024/Cookoff2024main.png';
 // **************************************************
 
+// faculty coordinator image import ********************
+import { faculty } from '../assets/leaders';
+// **************************************************
+
 // import other images here and export accordingly
 
 // exporting Home Page Gallery Images
@@ -42,3 +46,8 @@ export const CookOff2024MainImage = {
   url: CookOff2024Main,
   hashCode: 'LBIgv94n2,tSg400tSXU2WI9{+Sh',
 };
+
+export const FacultyImage = {
+  url: faculty,
+  hashCode: 'LnQu$zVt_4s:Pqoy%gbb$xjbMdae',
+}
