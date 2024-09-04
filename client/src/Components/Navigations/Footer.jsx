b9d8c931e5832ed542c50842a45caa4c12fa0f3a
tanujsinghchauhan <tanujsinghchauhan38@gmail.com>
2024-09-04 22:11:50 +0530

    about us content added

--- client/src/Components/Navigations/Footer.jsx
+++ client/src/Components/Navigations/Footer.jsx
@@ -1,6 +1,6 @@
 import { IoLogoInstagram } from "react-icons/io";
 import { CiLinkedin } from "react-icons/ci";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import { getAllBlogs } from "../../api/apiCall";
@@ -33,15 +33,25 @@ const Footer = () => {
   const displayBlogs = blogs.slice(0, 3).reverse();
 
   const handleBlogLink = (blog) => {
-    const formattedTitle = blog.heading.replace(/\s+/g, '-');
-    navigate(`/blogs/${formattedTitle}`, { state: { url: blog.imageUrl, hashCode: blog.imageHashCode, title: blog.heading, date: blog.date, about: blog.about, details: blog.blog[0].children[0].text, blogs: blogs } });
+    const formattedTitle = blog.heading.replace(/\s+/g, "-");
+    navigate(`/blogs/${formattedTitle}`, {
+      state: {
+        url: blog.imageUrl,
+        hashCode: blog.imageHashCode,
+        title: blog.heading,
+        date: blog.date,
+        about: blog.about,
+        details: blog.blog[0].children[0].text,
+        blogs: blogs,
+      },
+    });
     window.scrollTo(0, 0);
   };
 
   const defaultScrollToTop = () => {
     window.scrollTo(0, 0);
-  }
-  
+  };
+
   return (
     <>
       <footer>
@@ -76,24 +86,24 @@ const Footer = () => {
             </div>
             <div className="l_footer_list">
               {loading ? (
-                  <div className="flex justify-center items-center">
-                    <FaSpinner className="spinner text-center text-xl sm:text-3xl" />
+                <div className="flex justify-center items-center">
+                  <FaSpinner className="spinner text-center text-xl sm:text-3xl" />
+                </div>
+              ) : error ? (
+                <div className="flex">
+                  <ErrorBox />
+                </div>
+              ) : (
+                displayBlogs.map((blog, index) => (
+                  <div className="l_footer_link" key={index}>
+                    <span
+                      className="footer_link"
+                      onClick={() => handleBlogLink(blog)}
+                    >
+                      {blog.heading}
+                    </span>
                   </div>
-                ) : error ? (
-                  <div className="flex">
-                    <ErrorBox />
-                  </div>
-                ) : (
-                  displayBlogs.map((blog, index) => (
-                    <div className="l_footer_link" key={index}>
-                      <span 
-                        className="footer_link"
-                        onClick={() => handleBlogLink(blog)}
-                      >
-                        {blog.heading}
-                      </span>
-                    </div>
-                  ))
+                ))
               )}
             </div>
           </div>
@@ -111,6 +121,15 @@ const Footer = () => {
                   View Team
                 </Link>
               </div>
+              <div className="l_footer_link">
+                <Link
+                  to="/about-us"
+                  className="footer_link"
+                  onClick={defaultScrollToTop}
+                >
+                  About Us
+                </Link>
+              </div>
             </div>
           </div>
           <div className="col-sm-3 col-md-3 my-2">
@@ -119,12 +138,16 @@ const Footer = () => {
             </div>
             <div className="l_footer_list">
               <Link
-                to="https://www.instagram.com/codechef.vitc/" className="footer_link footer-icons" target="_blank"
+                to="https://www.instagram.com/codechef.vitc/"
+                className="footer_link footer-icons"
+                target="_blank"
               >
                 <IoLogoInstagram />
               </Link>
               <Link
-                to="https://www.linkedin.com/company/codechef-vit-chennai-chapter/" className="footer_link footer-icons" target="_blank"
+                to="https://www.linkedin.com/company/codechef-vit-chennai-chapter/"
+                className="footer_link footer-icons"
+                target="_blank"
               >
                 <CiLinkedin />
               </Link>
--- client/src/Pages/AboutUs.jsx
+++ client/src/Pages/AboutUs.jsx
@@ -21,14 +21,17 @@ function AboutUs() {
           About Us
         </h1>
         <p className="pt-3 pb-10 text-gray-600 leading-loose">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut facere ea,
-          rerum quo sunt cum pariatur consectetur voluptatibus ratione
-          voluptatem voluptates, perferendis quaerat vero? Obcaecati, in quidem?
-          Provident nobis libero voluptate itaque inventore fugiat iste
-          consectetur quod enim veniam, aspernatur nulla dolores maxime dicta!
-          Porro voluptates molestias impedit incidunt natus!
+          CodeChef VITC is a student chapter focused on helping and developing
+          eager students at our college with passion for coding and problem
+          solving. Here in CodeChef, our club stands united by shared commitment
+          to learning and excellence in programming make us more than just a
+          club at VITC. Our club is now focused on expanding into web
+          development and AI and ML, Competitive programming remains our primary
+          focus. Our regular coding contests and, hackathons, and workshops from
+          the basic to the advanced levels are designed to sharpen coders skills
+          and prepare them for both national and international competitions.
         </p>
-        <h1 className="text-3xl font-bold text-gray-800 pb-3 border-b border-gray-300 font-sans">
+        {/* <h1 className="text-3xl font-bold text-gray-800 pb-3 border-b border-gray-300 font-sans">
           Club Description
         </h1>
         <p className="pt-3 pb-10 text-gray-600 leading-loose">
@@ -38,20 +41,24 @@ function AboutUs() {
           neque voluptatum excepturi officiis omnis itaque, incidunt distinctio
           cupiditate iure. Fugiat cum aspernatur quis? Dolores dicta totam
           officia quo excepturi.
-        </p>
+        </p> */}
 
         <h1 className="text-3xl font-bold text-gray-800 pb-3 border-b border-gray-300 font-sans">
           Future Goals
         </h1>
         <p className="pt-3 pb-10 text-gray-600 leading-loose">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sunt,
-          nemo repellat tempora asperiores pariatur quasi consequuntur ipsam
-          fugiat facilis quia illum, enim impedit ea sit ipsa? Totam illum non
-          neque voluptatum excepturi officiis omnis itaque, incidunt distinctio
-          cupiditate iure. Fugiat cum aspernatur quis? Dolores dicta totam
-          officia quo excepturi.
+          CodeChef is dedicated to expanding its reach, ensuring that more
+          people discover the vibrant community we've built. Soon, our focus
+          will be on conducting a series of engaging events that cater to both
+          seasoned members and newcomers, fostering an environment where
+          everyone feels welcome. We aim to build a community that is not only
+          friendly and inclusive but also competitive, encouraging members to
+          challenge themselves and each other in a supportive atmosphere. Our
+          future goals are centred on creating meaningful connections, driving
+          participation, and establishing a club culture that thrives on both
+          camaraderie and healthy competition.
         </p>
-        <h1 className="text-3xl font-bold text-gray-800 pb-3 border-b border-gray-300 font-sans">
+        {/* <h1 className="text-3xl font-bold text-gray-800 pb-3 border-b border-gray-300 font-sans">
           Ongoing Works
         </h1>
         <ul className=" pt-3 pb-10 pl-6 list-disc space-y-4">
@@ -70,11 +77,11 @@ function AboutUs() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </span>
           </li>
-        </ul>
+        </ul>*/}
         <h1 className="text-3xl font-bold text-gray-800 pb-3 border-b border-gray-300 font-sans">
           Why Join Us?
         </h1>
-        <ul className="pt-3 pb-10 pl-6 list-disc space-y-4">
+        {/* <ul className="pt-3 pb-10 pl-6 list-disc space-y-4">
           <li>
             <span className="text-gray-700">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
@@ -93,7 +100,19 @@ function AboutUs() {
               ipsum dolor sit amet consectetur adipisicing elit.
             </span>
           </li>
-        </ul>
+        </ul> */}
+        <p className="pt-3 pb-10 text-gray-600 leading-loose">
+          Joining CodeChef offers a unique opportunity to be part of a dynamic
+          and supportive community where members share a passion for learning,
+          growth, and connection. Whether you're looking to enhance your skills,
+          make new friends, or challenge yourself in a competitive yet friendly
+          environment, our club provides the perfect platform. We host a variety
+          of engaging events that cater to diverse interests, ensuring that
+          there’s something for everyone, where you’ll gain access to networking
+          opportunities and a space where you can thrive both personally and
+          professionally. Become a part of our journey and help shape the future
+          of our community!
+        </p>
       </div>
     </div>
   );
