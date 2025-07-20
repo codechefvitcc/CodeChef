adfb28aec94b0e7c2fbe32dbd6763d05f2f099dc
Sasank-V <sasanklearner@gmail.com>
2025-07-20 20:07:14 +0530

    feat: add events and recruitments to sanity

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "codechef-blue": "#2e3444",
-      },
-    },
-
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
-  },
-  plugins: [],
-};
-
-export const theme = config.theme;
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "codechef-blue": "#2e3444",
+      },
+    },
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
+
+export const theme = config.theme;
 export default config;
